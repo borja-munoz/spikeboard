96f4a1f3f607d1880f1a9fc49e1354a178139104
Borja Muñoz <borja.munoz@gmail.com>
2026-04-18 12:52:19 +0200

    Allow landscape mode in Android

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
         theme_color: '#0f172a',
         background_color: '#0f172a',
         display: 'standalone',
-        orientation: 'portrait',
+        orientation: 'any',
         scope: '/spikeboard/',
         start_url: '/spikeboard/',
         icons: [
